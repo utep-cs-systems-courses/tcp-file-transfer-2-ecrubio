24f9c443f5746075aa5b6d5444eef0983d7d3817
ecrubio <ecrubio@miners.utep.edu>
2020-10-10 21:25:16 -0600

    server and sock updated

--- file-transfer-lab/f-transferServer.py
+++ file-transfer-lab/f-transferServer.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-import sys
+import sys, os
 sys.path.append("../lib")       # for params
 import re, socket, params
 
@@ -10,28 +10,43 @@ switchesVarDefaults = (
         (('-?', '--usage'), "usage", False), # boolean (set if present)
         )
 
+progname = "echoserver"
 paramMap = params.parseParams(switchesVarDefaults)
+
 debug, listenPort = paramMap['debug'], paramMap['listenPort']
 
 if paramMap['usage']:
         params.usage()
 
+#First the listening socket is created then binding the host with the port
+#number. The capacity of the listening socket is 5.
 lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # listener socket
 bindAddr = ("127.0.0.1", listenPort)
 lsock.bind(bindAddr)
 lsock.listen(5)
 print("listening on:", bindAddr)
 
-sock, addr = lsock.accept()
+#sock, addr = lsock.accept()
 
-print("connection rec'd from", addr)
+#print("connection rec'd from", addr)
 
 from framedSock import framedSend, framedReceive
 
 while True:
-    payload = framedReceive(sock, debug)
-    if debug: print("rec'd: ", payload)
-    if not payload:
-        break
-    payload += b"!"             # make emphatic!
-    framedSend(sock, payload, debug)
+        sock, addr = lsock.accept()
+        if not os.fork():
+                break
+        payload, contents = framedRecieve(sock, debug)
+        if not payload:
+                sys.exit(1)
+
+        filename = payload.decode()
+        if filename:
+                output = open(filename, 'wb')
+                output.write(contents)
+                output.close
+                print("File", filename, "accepted")
+                sys.exit(1)
+        else:
+                print("File", filename, "not accepted")
+                sys.exit(1)
